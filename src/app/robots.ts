fc6cebcad3e4b8786e801fc8b01f8f76dfac381d
Sanni, Olanrewaju <slanre26@gmail.com>
2026-04-11 16:13:44 +0100

    fixed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const siteUrl = "https://sutim-landing.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {
